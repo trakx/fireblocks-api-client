d4442d7d7304f326511791b5a895360af4cfab9f
AymericNoel <aym.noel@gmail.com>
2021-01-19 18:48:17 +0100

    Change way to get repository root in Secret.cs

--- src/Trakx.Fireblocks.ApiClient.Tests/Secrets.cs
+++ src/Trakx.Fireblocks.ApiClient.Tests/Secrets.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using Trakx.Utils.Extensions;
 
 namespace Trakx.Fireblocks.ApiClient.Tests
 {
@@ -7,10 +8,12 @@ namespace Trakx.Fireblocks.ApiClient.Tests
     {
         static Secrets()
         {
-            var srcPath = new DirectoryInfo(Environment.CurrentDirectory).Parent?.Parent?.Parent?.Parent;
+            var isRootDirectory = DirectoryInfoExtensions.TryWalkBackToRepositoryRoot(null, out var rootDirectory);
+            if (!isRootDirectory)
+                rootDirectory = null;
             try
             {
-                DotNetEnv.Env.Load(Path.Combine(srcPath?.FullName ?? string.Empty, ".env"));
+                DotNetEnv.Env.Load(Path.Combine(rootDirectory?.FullName ?? string.Empty, "src", ".env"));
             }
             catch (Exception)
             {
@@ -21,5 +24,5 @@ namespace Trakx.Fireblocks.ApiClient.Tests
         public static string FireblocksApiKey => Environment.GetEnvironmentVariable("FireblocksApiConfiguration__ApiKey")!;
         public static string FireblocksApiSecret => Environment.GetEnvironmentVariable("FireblocksApiConfiguration__ApiSecret")!;
     }
-    
+
 }
