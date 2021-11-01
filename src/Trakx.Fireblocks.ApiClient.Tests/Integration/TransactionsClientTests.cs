cc8c061cd270bee8ca44106b8f553e69a0b8b059
RafaelMeloTrakx <rafael@trakx.io>
2021-11-01 15:01:24 -0300

    Fixed bad nullability.

--- src/Trakx.Fireblocks.ApiClient.Tests/Integration/TransactionsClientTests.cs
+++ src/Trakx.Fireblocks.ApiClient.Tests/Integration/TransactionsClientTests.cs
@@ -11,11 +11,10 @@ namespace Trakx.Fireblocks.ApiClient.Tests.Integration
 {
     public class TransactionsClientTests : FireblocksClientTestsBase
     {
-        
         private readonly ITransactionsClient _fireblocksClient;
         private readonly MockCreator _mockCreator;
-        
-        public TransactionsClientTests(FireblocksApiFixture apiFixture, ITestOutputHelper output) 
+
+        public TransactionsClientTests(FireblocksApiFixture apiFixture, ITestOutputHelper output)
             : base(apiFixture, output)
         {
             _mockCreator = new MockCreator(output);
@@ -26,11 +25,11 @@ namespace Trakx.Fireblocks.ApiClient.Tests.Integration
         public async Task GetTransactionsAsync_should_query_ethereum_transactions_when_passing_eth_asset_id()
         {
             var assetId = "ETH";
-            var response = await _fireblocksClient.GetTransactionsAsync(assets: assetId, limit: 2 );
+            var response = await _fireblocksClient.GetTransactionsAsync(assets: assetId, limit: 2);
             response.Result.Should().NotBeEmpty();
             response.Result.ForEach(f => f.AssetId.Should().Be(assetId));
         }
-        
+
         [Fact]
         public async Task TransactionsAllAsync_should_query_ethereum_transactions_when_passing_eth_asset_id()
         {
@@ -38,6 +37,5 @@ namespace Trakx.Fireblocks.ApiClient.Tests.Integration
             var response = await _fireblocksClient.CreateTransactionAsync(transaction);
             response.Result.Status.Should().Be("ok");
         }
-        
     }
 }
