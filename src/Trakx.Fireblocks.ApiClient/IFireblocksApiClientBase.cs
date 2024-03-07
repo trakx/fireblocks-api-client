a31b3d7c572f3f797131bd9952f2d7be478691e6
João Pedro Vieira <joao.fbvieira@gmail.com>
2024-03-07 15:22:27 +0000

    New IFireblocksApiClientBase base type

---
+++ src/Trakx.Fireblocks.ApiClient/IFireblocksApiClientBase.cs
@@ -0,0 +1,9 @@
+namespace Trakx.Fireblocks.ApiClient;
+
+/// <summary>
+/// Base interface for all ApiClients.
+/// Used to inject the ApiClient in the ApiClientFactory and to register the ApiClient in the DI container.
+/// </summary>
+public interface IFireblocksApiClientBase
+{
+}
