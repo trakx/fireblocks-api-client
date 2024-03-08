017ff1f3b9d2fbb7a09d863211db3572c34b8064
João Pedro Vieira <joao.fbvieira@gmail.com>
2024-03-08 16:23:20 +0000

    Updated T4 template for partial classes

--- src/Trakx.Fireblocks.ApiClient/FireblocksApiClients.Partials.cs
+++ src/Trakx.Fireblocks.ApiClient/FireblocksApiClients.Partials.cs
@@ -1,7 +1,7 @@
 ﻿#pragma warning disable S1172, IDE0060 // Unused method parameters should be removed
 
 namespace Trakx.Fireblocks.ApiClient;
-/*
+
 internal partial class Audit_LogsClient
 {
     partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
@@ -9,6 +9,7 @@ internal partial class Audit_LogsClient
         CredentialsProvider.AddCredentials(request);
     }
 }
+
 internal partial class ContractsClient
 {
     partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
@@ -16,6 +17,7 @@ internal partial class ContractsClient
         CredentialsProvider.AddCredentials(request);
     }
 }
+
 internal partial class Exchange_accountsClient
 {
     partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
@@ -23,6 +25,7 @@ internal partial class Exchange_accountsClient
         CredentialsProvider.AddCredentials(request);
     }
 }
+
 internal partial class External_walletsClient
 {
     partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
@@ -38,6 +41,7 @@ internal partial class Fiat_accountsClient
         CredentialsProvider.AddCredentials(request);
     }
 }
+
 internal partial class Gas_stationsClient
 {
     partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
@@ -45,6 +49,7 @@ internal partial class Gas_stationsClient
         CredentialsProvider.AddCredentials(request);
     }
 }
+
 internal partial class Internal_walletsClient
 {
     partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
@@ -60,6 +65,7 @@ internal partial class Network_connectionsClient
         CredentialsProvider.AddCredentials(request);
     }
 }
+
 internal partial class Off_exchangesClient
 {
     partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
@@ -68,13 +74,6 @@ internal partial class Off_exchangesClient
     }
 }
 
-internal partial class Supported_assetsClient
-{
-    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
-    {
-        CredentialsProvider.AddCredentials(request);
-    }
-}
 internal partial class TransactionsClient
 {
     partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
@@ -90,6 +89,7 @@ internal partial class UsersClient
         CredentialsProvider.AddCredentials(request);
     }
 }
+
 internal partial class VaultsClient
 {
     partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
@@ -97,6 +97,7 @@ internal partial class VaultsClient
         CredentialsProvider.AddCredentials(request);
     }
 }
+
 internal partial class WebhooksClient
 {
     partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
@@ -104,13 +105,7 @@ internal partial class WebhooksClient
         CredentialsProvider.AddCredentials(request);
     }
 }
-internal partial class NFTs_BetaClient
-{
-    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
-    {
-        CredentialsProvider.AddCredentials(request);
-    }
-}
+
 internal partial class Web3_connectionsClient
 {
     partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
@@ -118,13 +113,7 @@ internal partial class Web3_connectionsClient
         CredentialsProvider.AddCredentials(request);
     }
 }
-internal partial class Payments__Cross_Border_settlementClient
-{
-    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
-    {
-        CredentialsProvider.AddCredentials(request);
-    }
-}
+
 internal partial class Payments__PayoutClient
 {
     partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
@@ -132,10 +121,155 @@ internal partial class Payments__PayoutClient
         CredentialsProvider.AddCredentials(request);
     }
 }
-internal partial class Travel_Rule_BetaClient
+
+internal partial class AssetsClient
 {
     partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
     {
         CredentialsProvider.AddCredentials(request);
     }
-}*/
+}
+
+internal partial class WorkspaceClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class Blockchains_and_assetsClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class User_groups__BetaClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class Staking__BetaClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class Admin_QuorumClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class NFTsClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class Travel_Rule__BetaClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class ComplianceClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class Compliance_Screening_ConfigurationClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class OTA__BetaClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class Workspace_Status__BetaClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class Policy_Editor__BetaClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class Console_UserClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class Api_UserClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class Reset_deviceClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class Whitelist_ip_addressesClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class Smart_TransferClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
+
+internal partial class JobsClient
+{
+    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+    {
+        CredentialsProvider.AddCredentials(request);
+    }
+}
