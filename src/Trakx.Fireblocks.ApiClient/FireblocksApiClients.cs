0fe64ffd92c016462b04cecc133cf40235a6418b
Gonen <goneneren@gmail.com>
2024-05-28 23:24:18 +0700

    Added Kucoin

--- src/Trakx.Fireblocks.ApiClient/FireblocksApiClients.cs
+++ src/Trakx.Fireblocks.ApiClient/FireblocksApiClients.cs
@@ -26805,6 +26805,9 @@ namespace Trakx.Fireblocks.ApiClient
         [System.Runtime.Serialization.EnumMember(Value = @"GATEIO")]
         GATEIO = 26,
 
+        [System.Runtime.Serialization.EnumMember(Value = @"KUCOIN")]
+        KUCOIN = 27,
+
     }
 
     [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.0.2.0 (NJsonSchema v11.0.0.0 (Newtonsoft.Json v13.0.0.0))")]
