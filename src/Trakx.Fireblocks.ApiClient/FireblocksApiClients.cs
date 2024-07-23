c2c1e5201e686e35169ae55e78f6e8459cb3739d
João Pedro Vieira <joao.fbvieira@gmail.com>
2024-07-23 11:22:17 +0100

    new enum

--- src/Trakx.Fireblocks.ApiClient/FireblocksApiClients.cs
+++ src/Trakx.Fireblocks.ApiClient/FireblocksApiClients.cs
@@ -35338,6 +35338,12 @@ namespace Trakx.Fireblocks.ApiClient
         [System.Runtime.Serialization.EnumMember(Value = @"LEGACY")]
         LEGACY = 1,
 
+        [System.Runtime.Serialization.EnumMember(Value = @"BASE")]
+        BASE = 2,
+
+        [System.Runtime.Serialization.EnumMember(Value = @"PAYMENT")]
+        PAYMENT = 3,
+
     }
 
     [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.0.2.0 (NJsonSchema v11.0.0.0 (Newtonsoft.Json v13.0.0.0))")]
